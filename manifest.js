9dd56f41307d3312245377b7723e45fa717e3f8a
Югов Максим <maksim.yugov.89@gmail.com>
2025-09-14 00:40:48 +0200

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,15 +1,15 @@
 {
-  "name": "FuelMaster",
-  "short_name": "FuelMaster",
-  "start_url": "index.html",
-  "display": "standalone",
-  "background_color": "#2c2c3e",
-  "theme_color": "#007bff",
-  "icons": [
-    {
-      "src": "assets/img/logo.PNG",
-      "sizes": "192x192",
-      "type": "image/png"
-    }
-  ]
+    "name": "FuelMaster",
+    "short_name": "FuelMaster",
+    "start_url": "index.html",
+    "display": "standalone",
+    "background_color": "#2c2c3e",
+    "theme_color": "#007bff",
+    "icons": [
+        {
+            "src": "assets/img/logo.PNG",
+            "sizes": "192x192",
+            "type": "image/png"
+        }
+    ]
 }
