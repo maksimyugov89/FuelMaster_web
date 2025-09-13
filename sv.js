d4b1f2a8aab05b3606f12bc19f2c8d96e67363d5
Югов Максим <maksim.yugov.89@gmail.com>
2025-09-13 22:42:28 +0200

    Update sv.js

--- sv.js
+++ sv.js
@@ -1,32 +1,43 @@
-const CACHE_NAME = "fuelmaster-v1";
-const ASSETS = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/assets/css/styles.css",
-  "/assets/js/script.js",
-  "/assets/img/logo.png",
-  "/assets/img/icon-192.png",
-  "/assets/img/icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('fuelmaster-v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/assets/css/styles.css',
+                '/assets/js/script.js',
+                '/manifest.json',
+                '/assets/img/logo.PNG',
+                '/assets/img/Screenshot-1-dark.jpg',
+                '/assets/img/Screenshot-2-dark.jpg',
+                '/assets/img/Screenshot-3-dark.jpg',
+                '/assets/img/Screenshot-4-dark.jpg',
+                '/assets/img/Screenshot-5-dark.jpg',
+                '/assets/img/Screenshot-6-dark.jpg',
+                '/assets/img/Screenshot-7-dark.jpg',
+                '/assets/img/Screenshot-8-dark.jpg',
+                '/assets/img/Screenshot-9-dark.jpg',
+                '/assets/img/Screenshot-10-dark.jpg',
+                '/assets/img/Screenshot-11-dark.jpg',
+                '/assets/img/Screenshot-12-dark.jpg',
+                '/assets/img/Screenshot-1-light.jpg',
+                '/assets/img/Screenshot-2-light.jpg',
+                '/assets/img/Screenshot-3-light.jpg',
+                '/assets/img/Screenshot-4-light.jpg',
+                '/assets/img/Screenshot-5-light.jpg',
+                '/assets/img/Screenshot-6-light.jpg',
+                '/assets/img/Screenshot-7-light.jpg',
+                '/assets/img/Screenshot-8-light.jpg',
+                '/assets/img/Screenshot-9-light.jpg'
+            ]);
+        })
+    );
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
